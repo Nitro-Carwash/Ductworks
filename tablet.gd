a87c42f80a92c6441ff73d464e3a32c1229b53b4
Nitro Carwash <>
2024-02-29 20:59:53 -0800

    Put tablet in hand

--- tablet.gd
+++ tablet.gd
@@ -1,12 +1,11 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	# Godot currently gives a nonsense error if you try to set the viewport material via the inspector, so we'll wait two frames and then create a new material and set it ourselves.
 	# The error doesn't actually seem to impact anything? But why risk it
 	# The side-effect is that now we have to make any edits to this material in code instead of the inspector...
-	# Bug link: https://github.com/godotengine/godot/issues/66247
+	# Tracker link: https://github.com/godotengine/godot/issues/66247
 	
 	# Let two frames pass to make sure the viewport is captured.
 	await get_tree().process_frame
@@ -16,4 +15,4 @@ func _ready():
 	var viewport_material : StandardMaterial3D = StandardMaterial3D.new()
 	viewport_material.albedo_texture = $TabletViewport.get_texture()
 	viewport_material.albedo_texture_force_srgb = true
-	$TabletQuad.set_surface_override_material(0, viewport_material)
+	$TabletScreenMesh.set_surface_override_material(0, viewport_material)
