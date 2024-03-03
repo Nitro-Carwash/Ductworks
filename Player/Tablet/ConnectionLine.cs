d41597a654171ff8df2f81d2dfc2c589e4329492
Nitro Carwash <>
2024-03-02 20:20:18 -0800

    Add collision to connections

--- Player/Tablet/ConnectionLine.cs
+++ Player/Tablet/ConnectionLine.cs
@@ -6,6 +6,8 @@ public partial class ConnectionLine : Line2D
 {
 	public bool enabled;
 
+	public Area2D Area2D;
+
 	private Vector2 startPos;
 
 	public override void _Ready()
@@ -35,5 +37,20 @@ public partial class ConnectionLine : Line2D
 		this.AddPoint(this.startPos);
 		this.AddPoint(pos);
 		this.enabled = false;
+		
+		// Add collision
+		var collision = new CollisionShape2D();
+		var a = this.Points[0];
+		var b = this.Points[1];
+		collision.Position = (a + b) / 2;
+		collision.Rotation = a.DirectionTo(b).Angle();
+		
+		var collisionShape = new RectangleShape2D();
+		collisionShape.Size = new Vector2(a.DistanceTo(b), this.Width);
+		collision.Shape = collisionShape;
+
+		this.Area2D = new Area2D();
+		this.Area2D.AddChild(collision);
+		this.AddChild(this.Area2D);
 	}
 }
